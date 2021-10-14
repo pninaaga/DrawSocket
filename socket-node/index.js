2d3938ce83155fd87e1a7c8096e74e9aeb1f7c14
unknown <pnina@leader.codes>
2021-10-14 15:47:26 +0300

    fixed cors

--- socket-node/index.js
+++ socket-node/index.js
@@ -2,11 +2,17 @@
 const express = require('express');
 const cors = require('cors')
 const app = express();
-const server = app.listen(3000, () => { console.log('connect') });
+const server = app.listen(8080, () => { console.log('connect') });
 const http = require('http');
-const io = require("socket.io")(server)
 
 app.use(cors())
+
+const io = require("socket.io")(server,{
+    cors:{
+        origin: "http://localhost:3000",
+        methods: ["GET","POST"]
+    }
+})
 
 let arrPointsDraw = []
 let arrNames = []
